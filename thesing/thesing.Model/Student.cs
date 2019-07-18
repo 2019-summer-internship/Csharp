22b2daa182bb56bf0ff7307d2df48a386ba66c04
smileswjtu <2530095054@qq.com>
2019-07-18 18:00:31 +0800

    用户

---
+++ thesing/thesing.Model/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace thesing.Model
+{
+    class Student
+    {
+        public String studentID;
+    }
+}
---
+++ thesing/thesing.Model/user.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace thesing.Model
+{
+    public class User
+    {
+        /// <summary>
+        /// 用户编号
+        /// </summary>
+        public String ID ;
+        /// <summary>
+        /// 用户姓名
+        /// </summary>
+        public String name;
+        /// <summary>
+        /// 用户性别
+        /// </summary>
+        public String gender;
+        /// <summary>
+        /// 
+        /// </summary>
+        public String password;
+        /// <summary>
+        /// 头像
+        /// </summary>
+        public String avatar;
+        /// <summary>
+        /// 学生【STUDENT】导师【TEACHER】学院【SCHOOL】校方【UNIVERSITY】
+        /// </summary>
+        public int type;
+        /// <summary>
+        /// 电话
+        /// </summary>
+        public String phone;
+        /// <summary>
+        /// 邮箱号
+        /// </summary>
+        public String email;
+
+
+
+    }
+}
