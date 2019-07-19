babc4a6fdb8e449b9e2fe409ccadc2af3b65dd8d
Artisigngineer <39426035+Artisigngineer@users.noreply.github.com>
2019-07-19 16:33:11 +0800

    修复重命名错误

--- thesing/thesing.Model/Student.cs
+++ thesing/thesing.Model/Student.cs
@@ -12,53 +12,48 @@ namespace thesing.Model
         /// <summary>
         /// 学生id
         /// </summary>
-      
+        public string studentID;
 
-            /// <summary>
-            /// 学生id
-            /// </summary>
-            public string studentID;
+        /// <summary>
+        /// 成绩列表
+        /// </summary>
+        public List<Achievement> achievement;
 
-            /// <summary>
-            /// 成绩列表
-            /// </summary>
-            public List<Achievement> achievement;
+        /// <summary>
+        /// 班级
+        /// </summary>
+        public string clas;
 
-            /// <summary>
-            /// 班级
-            /// </summary>
-            public string clas;
+        /// <summary>
+        /// 专业
+        /// </summary>
+        public string major;
 
-            /// <summary>
-            /// 专业
-            /// </summary>
-            public string major;
+        /// <summary>
+        /// 学院
+        /// </summary>
+        public string school;
 
-            /// <summary>
-            /// 学院
-            /// </summary>
-            public string school;
+        /// <summary>
+        /// <summary>
+        /// 我的题目
+        /// </summary>
+        public Project project;
 
-            /// <summary>
-            /// <summary>
-            /// 我的题目
-            /// </summary>
-            public Project project;
+        /// <summary>
+        /// 我的老师
+        /// </summary>
+        public string teacher;
 
-            /// <summary>
-            /// 我的老师
-            /// </summary>
-            public string teacher;
+        /// <summary>
+        /// 我的进度
+        /// </summary>
+        public List<Task> process;
 
-            /// <summary>
-            /// 我的进度
-            /// </summary>
-            public List<Task> process;
-
-            /// <summary>
-            /// 通知的集合
-            /// </summary>
-            public List<Mail> mail;
+        /// <summary>
+        /// 通知的集合
+        /// </summary>
+        public List<Mail> mail;
         
     }
 }
