c1c665c62d08bcdefbf5631549ba459123f849e2
Artisigngineer <39426035+Artisigngineer@users.noreply.github.com>
2019-07-19 22:13:36 +0800

    删除项目

--- thesing/controller/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Controller
-{
-    public class Class1
-    {
-    }
-}
--- thesing/thesing.Service/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace thesing.Service
-{
-    public class user
-    {
-    }
-}
