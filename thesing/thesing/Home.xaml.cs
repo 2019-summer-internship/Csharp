c16db5b7677017806fbf7a4b788a437572d203c8
Artisigngineer <39426035+Artisigngineer@users.noreply.github.com>
2019-07-21 01:41:05 +0800

    实现菜单项互斥

--- thesing/thesing/Home.xaml.cs
+++ thesing/thesing/Home.xaml.cs
@@ -19,6 +19,11 @@ namespace thesing
     /// </summary>
     public partial class Home : Window
     {
+        /// <summary>
+        /// 所有子菜单
+        /// </summary>
+        List<MenuItem> allMenu = new List<MenuItem>();
+
         public Home(Model.User user)
         {
             InitializeComponent();
@@ -46,8 +51,8 @@ namespace thesing
             student_project_menu.Visibility = Visibility.Visible;
             student_score_menu.Visibility = Visibility.Visible;
             student_teacher_menu.Visibility = Visibility.Visible;
-
-            student_project_menu.IsChecked = true;
+            student_project_menu.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#EBEBEB"));
+            student_project_menu.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#333333"));
         }
 
         /// <summary>
@@ -60,8 +65,8 @@ namespace thesing
             teacher_examine_menu.Visibility = Visibility.Visible;
             teacher_project_menu.Visibility = Visibility.Visible;
             teacher_student_menu.Visibility = Visibility.Visible;
-
-            teacher_project_menu.IsChecked = true;
+            teacher_project_menu.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#EBEBEB"));
+            teacher_project_menu.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#333333"));
         }
         
         /// <summary>
@@ -75,8 +80,8 @@ namespace thesing
             school_reconsider_menu.Visibility = Visibility.Visible;
             school_schedule_menu.Visibility = Visibility.Visible;
             school_teacher_menu.Visibility = Visibility.Visible;
-
-            school_schedule_menu.IsChecked = true;
+            school_schedule_menu.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#EBEBEB"));
+            school_schedule_menu.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#333333"));
         }
 
         /// <summary>
@@ -88,8 +93,57 @@ namespace thesing
             nickname.Text = user.name;
             university_plan_menu.Visibility = Visibility.Visible;
             university_school_menu.Visibility = Visibility.Visible;
-
-            university_school_menu.IsChecked = true;
+            university_school_menu.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#EBEBEB"));
+            university_school_menu.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#333333"));
         }
+
+        /// <summary>
+        /// 菜单点击函数
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void mainMenuClick(object sender, RoutedEventArgs e)
+        {
+            MenuItem menuItem = sender as MenuItem;
+            foreach(MenuItem menu in allMenu)
+            {
+                menu.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#66CCFF"));
+                menu.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFFFFF"));
+            }
+            menuItem.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#EBEBEB"));
+            menuItem.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#333333"));
+            MessageBox.Show(menuItem.Name.ToString());
+        }
+
+
+        private void getAllMenu(object sender, RoutedEventArgs e)
+        {
+            allMenu = GetChildObjects<MenuItem>(mainMenu, typeof(MenuItem));
+        }
+
+        /// <summary>
+        /// 获取所有子元素
+        /// </summary>
+        /// <typeparam name="MenuItem"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public List<T> GetChildObjects<T>(DependencyObject obj, Type typename) where T : FrameworkElement
+        {
+            DependencyObject child = null;
+            List<T> childList = new List<T>();
+
+            for (int i = 0; i <= VisualTreeHelper.GetChildrenCount(obj) - 1; i++)
+            {
+                child = VisualTreeHelper.GetChild(obj, i);
+
+                if (child is T && (((T)child).GetType() == typename))
+                {
+                    childList.Add((T)child);
+                }
+                childList.AddRange(GetChildObjects<T>(child, typename));
+            }
+            return childList;
+        }
+
     }
 }
