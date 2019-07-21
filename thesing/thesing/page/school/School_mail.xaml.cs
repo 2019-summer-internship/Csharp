a2c559c1d962508153cf86037dbdbd1e415466cb
smileswjtu <2530095054@qq.com>
2019-07-21 10:08:05 +0800

    登录界面，学院端整合完毕

---
+++ thesing/thesing/page/school/School_mail.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace thesing.page.school
+{
+    /// <summary>
+    /// School_mail.xaml 的交互逻辑
+    /// </summary>
+    public partial class School_mail : Page
+    {
+        public School_mail()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ thesing/thesing/page/school/School_project.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace thesing.page.school
+{
+    /// <summary>
+    /// School_project.xaml 的交互逻辑
+    /// </summary>
+    public partial class School_project : Page
+    {
+        public School_project()
+        {
+            InitializeComponent();
+        }
+    }
+}
