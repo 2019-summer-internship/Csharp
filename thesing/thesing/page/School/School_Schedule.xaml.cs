8f77c7c5cb5f010d822f20c8fc25e245996b71be
smileswjtu <2530095054@qq.com>
2019-07-21 20:44:06 +0800

    疑似卡片和个人信息页

--- thesing/thesing/page/School/School_Schedule.xaml.cs
+++ thesing/thesing/page/School/School_Schedule.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,23 @@ namespace thesing.page.school
     /// </summary>
     public partial class School_shdule : Page
     {
+        public ObservableCollection<Person> PList { get; set; }
         public School_shdule()
         {
             InitializeComponent();
+            PList = new ObservableCollection<Person>();
+            PList.Add(new Person { IsFemale = true, Name = "小红" });
+            PList.Add(new Person { IsFemale = false, Name = "小明" });
+            PList.Add(new Person { IsFemale = false, Name = "小X" });
+            PList.Add(new Person { IsFemale = false, Name = "小xx" });
+
+            this.DataContext = this;
         }
+        public class Person
+        {
+            public bool IsFemale { get; set; }
+            public string Name { get; set; }
+        }
+
     }
 }
