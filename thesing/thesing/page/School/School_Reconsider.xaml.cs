38addef23f9d55cfdf586608e9d6ac0ad7d2138a
smileswjtu <2530095054@qq.com>
2019-07-22 16:13:30 +0800

    顶部搜索栏确定

--- thesing/thesing/page/School/School_Reconsider.xaml.cs
+++ thesing/thesing/page/School/School_Reconsider.xaml.cs
@@ -24,5 +24,10 @@ namespace thesing.page.school
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
